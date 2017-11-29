e1d9f2247c1c2696f520ff68e5ac824bbc8111a3
Sergey Tepliakov <seteplia@microsoft.com>
2017-11-28 17:48:37 -0800

    Add AsyncVoid project

---
+++ src/01_AsyncVoidBuilder/AsyncVoidSample.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _01_AsyncVoidBuilder
+{
+    [TestFixture]
+    public class AsyncVoidSample
+    {
+        [Test]
+        public void RunAsyncVoid()
+        {
+            AsyncVoidMethodBuilder.Inspector.Clear();
+            VoidAsync();
+            Thread.Sleep(42);
+            AsyncVoidMethodBuilder.Inspector.Print();
+
+            Assert.AreEqual(
+                new string[]
+                {
+                    ".ctor", // AsyncVoidMethodBuilder.ctor
+                    "Start", // AsyncVoidMethodBuilder.Start
+                    // Here the method builder calls the MoveNext on the state machine,
+                    // and the state machine runs the code from the method body.
+                    "VoidAsync_Start",
+                    // The state machine notifies the builder that we're done,
+                    // because method is finished synchronously
+                    "SetResult",
+                },
+                AsyncVoidMethodBuilder.Inspector.InvokedMembers);
+        }
+
+        [Test]
+        public void RunAsyncVoidWithAwait()
+        {
+            AsyncVoidMethodBuilder.Inspector.Clear();
+            VoidAsyncWithAwait();
+            Thread.Sleep(42);
+            AsyncVoidMethodBuilder.Inspector.Print();
+
+            Assert.AreEqual(
+                new string[]
+                {
+                    ".ctor", // AsyncVoidMethodBuilder.ctor
+                    "Start", // AsyncVoidMethodBuilder.Start
+                    // Here the method builder calls the MoveNext on the state machine
+                    "VoidAsync_Start",
+                    // Task.Yield is finished, and the state machine calls Builder.AwaitUnsafeOnCompleted
+                    "AwaitUnsafeOnCompleted",
+
+                    // The builder moves the state machine
+                    "VoidAsync_AfterAwait",
+
+                    // The state machine notifies the builder that we're done
+                    "SetResult",
+                },
+                AsyncVoidMethodBuilder.Inspector.InvokedMembers);
+        }
+
+        [Test]
+        public void RunAsyncVoidThatFails()
+        {
+            AsyncVoidMethodBuilder.Inspector.Clear();
+            VoidAsyncThatThrows();
+            Thread.Sleep(42);
+            AsyncVoidMethodBuilder.Inspector.Print();
+
+            Assert.AreEqual(
+                new string[]
+                {
+                    ".ctor", // AsyncVoidMethodBuilder.ctor
+                    "Start", // AsyncVoidMethodBuilder.Start
+                    // Here the method builder calls the MoveNext on the state machine
+                    "VoidAsync_Start",
+                    
+                    // The state machine calls SetException on the builder instance
+                    "SetException",
+                },
+                AsyncVoidMethodBuilder.Inspector.InvokedMembers);
+        }
+
+        [Test]
+        public void RunAsyncVoidWithCancellation()
+        {
+            AsyncVoidMethodBuilder.Inspector.Clear();
+            VoidAsyncWithCancellation();
+            Thread.Sleep(42);
+            AsyncVoidMethodBuilder.Inspector.Print();
+
+            Assert.AreEqual(
+                new string[]
+                {
+                    ".ctor", // AsyncVoidMethodBuilder.ctor
+                    "Start", // AsyncVoidMethodBuilder.Start
+                    // Here the method builder calls the MoveNext on the state machine
+                    "VoidAsync_Start",
+                    
+                    // The state machine calls SetException on the builder instance with TaskCanceledException
+                    "SetException",
+                },
+                AsyncVoidMethodBuilder.Inspector.InvokedMembers);
+
+            Assert.IsInstanceOf<TaskCanceledException>(AsyncVoidMethodBuilder.LastInstance.Exception);
+        }
+
+        public async void VoidAsync()
+        {
+            AsyncVoidMethodBuilder.Inspector.Record("VoidAsync_Start");
+        }
+
+        public async void VoidAsyncWithAwait()
+        {
+            AsyncVoidMethodBuilder.Inspector.Record("VoidAsync_Start");
+            await Task.Yield();
+            AsyncVoidMethodBuilder.Inspector.Record("VoidAsync_AfterAwait");
+        }
+
+        public async void VoidAsyncThatThrows()
+        {
+            AsyncVoidMethodBuilder.Inspector.Record("VoidAsync_Start");
+            throw new InvalidOperationException();
+        }
+
+        public async void VoidAsyncWithCancellation()
+        {
+            AsyncVoidMethodBuilder.Inspector.Record("VoidAsync_Start");
+
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+
+            await Task.Delay(42, cts.Token);
+        }
+    }
+}
---
+++ src/Utilities/Inspector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public sealed class Inspector
+    {
+        public List<string> InvokedMembers { get; } = new List<string>();
+
+        public void Clear() => InvokedMembers.Clear();
+
+        public void Print()
+        {
+            foreach (var m in InvokedMembers)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        public void RecordInvocation([CallerMemberName]string member = null)
+        {
+            InvokedMembers.Add(member);
+        }
+
+        public void Record(string message)
+        {
+            InvokedMembers.Add(message);
+        }
+    }
+}
